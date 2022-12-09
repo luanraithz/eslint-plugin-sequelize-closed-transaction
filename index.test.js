9d87cc6df0b9ea650b5b6445ecb01c1ddbc83899
luanraithz <luanraithz@gmail.com>
2022-12-09 16:09:56 -0300

    format

--- index.test.js
+++ index.test.js
@@ -8,55 +8,59 @@ const ruleTester = new RuleTester({
 ruleTester.run("transaction", rule.rules.transaction, {
     valid: [
         {
-            code: `async function simple() {
-            const transaction = await sequelize.transaction();
-            if (false) {
-              await transaction.rollback()
-              return
-            }
-            await transaction.rollback()
-            }`,
+            code: `
+async function simple() {
+  const transaction = await sequelize.transaction();
+  if (false) {
+    await transaction.rollback()
+    return
+  }
+  await transaction.rollback()
+}`,
         },
         {
-            code: `async function withTryAndFinally() {
-            const transaction = await sequelize.transaction();
-            try {
-              return 1
-            } catch (err) {
-              console.error()
-            } finally {
-              await transaction.rollback()
-            }
-            }`,
+            code: `
+async function withTryAndFinally() {
+  const transaction = await sequelize.transaction();
+  try {
+    return 1
+  } catch (err) {
+    console.error()
+  } finally {
+    await transaction.rollback()
+  }
+}`,
         },
         {
-            code: `async function nested() {
-            const transaction = await sequelize.transaction();
-            {
-              {
-                {
-                  await transaction.rollback()
-                }
-              }
-            }
-            }`,
+            code: `
+async function nested() {
+  const transaction = await sequelize.transaction();
+  {
+    {
+      {
+        await transaction.rollback()
+      }
+    }
+  }
+}`,
         },
       {
-            code: `async function shadowing() {
-            const transaction = await sequelize.transaction();
-            if (123) {
-              await transaction.rollback()
-              return bla
-            } else {
-              await transaction.rollback()
-            }
-
-            }`,
+            code: `
+async function ifElse() {
+  const transaction = await sequelize.transaction();
+  if (123) {
+    await transaction.rollback()
+    return bla
+  } else {
+    await transaction.rollback()
+  }
+}`,
       }
     ],
     invalid: [
         {
-            code: `async function nestedStatements() {
+            code: `
+async function nestedStatements() {
   let transaction = await sequelize.transaction();
   if (3 > 2) {
     if (4 > 2) {
@@ -69,11 +73,12 @@ ruleTester.run("transaction", rule.rules.transaction, {
             errors: [{ message: "Transaction in this context is not closed at some path." }]
         },
         {
-            code: `async function shadowing() {
-            const transaction = await sequelize.transaction();
+            code: `
+async function noNesting() {
+  const transaction = await sequelize.transaction();
 
-            return await fn(1)
-            }`,
+  return await 1
+}`,
             errors: [{ message: "Transaction in this context is not closed at some path." }]
         },
     ]
